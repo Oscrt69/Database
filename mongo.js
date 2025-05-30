668a01aafc345ddcc8737ea2e5cdbb698ce2300e
Oscarrrrrrrrrrrrrrrr <oscaryavat3@gmail.com>
2025-05-30 14:45:55 +0700

    Update mongo.js

--- mongo.js
+++ mongo.js
@@ -1,5 +1,18 @@
 Use WarungPakBudi_5027241053;
 
+//buat 3 koleksi
 db.produk
 db.transaksi
 db.pelanggan
+
+// mencari kategori minuman dengan stok kurang dari 10
+db.produk.find({
+  kategori: "Minuman",
+  stok: { $lt: 10 }
+});
+
+// Tampilkan data pelanggan “Siti Aminah”
+db.pelanggan.find({
+  nama_pelanggan: "Siti Aminah"
+});
+
